cb2d34bb84f09cde142914990cc96c35c952cfc0
slmt <sam123456777@gmail.com>
2017-07-26 16:26:20 +0800

    Add test cases for multi-key indexes

--- src/test/java/org/vanilladb/core/storage/index/hash/HashIndexTest.java
+++ src/test/java/org/vanilladb/core/storage/index/hash/HashIndexTest.java
@@ -72,17 +72,6 @@ public class HashIndexTest {
 		schema.addField("deptid", INTEGER);
 		md.createTable(dataTableName, schema, tx);
 		
-		List<String> idxFlds1 = new LinkedList<String>();
-		idxFlds1.add("cid");
-		List<String> idxFlds2 = new LinkedList<String>();
-		idxFlds2.add("title");
-		List<String> idxFlds3 = new LinkedList<String>();
-		idxFlds3.add("deptid");
-		
-		md.createIndex("_tempHI1", dataTableName, idxFlds1, IndexType.HASH, tx);
-		md.createIndex("_tempHI2", dataTableName, idxFlds2, IndexType.HASH, tx);
-		md.createIndex("_tempHI3", dataTableName, idxFlds3, IndexType.HASH, tx);
-
 		tx.commit();
 	}
 	
@@ -92,6 +81,29 @@ public class HashIndexTest {
 		
 		if (logger.isLoggable(Level.INFO))
 			logger.info("FINISH HASH INDEX TEST");
+	}
+	
+	private static void createSingleKeyIndex() {
+		Transaction tx = VanillaDb.txMgr().newTransaction(
+				Connection.TRANSACTION_SERIALIZABLE, false);
+		
+		List<String> idxFlds = new LinkedList<String>();
+		idxFlds.add("cid");
+		md.createIndex("_tempH_SI1", dataTableName, idxFlds, IndexType.HASH, tx);
+		
+		tx.commit();
+	}
+	
+	private static void createMultiKeyIndex() {
+		Transaction tx = VanillaDb.txMgr().newTransaction(
+				Connection.TRANSACTION_SERIALIZABLE, false);
+		
+		List<String> idxFlds = new LinkedList<String>();
+		idxFlds.add("cid");
+		idxFlds.add("deptid");
+		md.createIndex("_tempH_MI1", dataTableName, idxFlds, IndexType.HASH, tx);
+		
+		tx.commit();
 	}
 	
 	@Before
@@ -107,9 +119,12 @@ public class HashIndexTest {
 	}
 
 	@Test
-	public void testHashIndex() {
+	public void testBasicOperations() {
+		
+		createSingleKeyIndex();
+		
 		List<IndexInfo> idxList = md.getIndexInfo(dataTableName, "cid", tx);
-		Index cidIndex = idxList.get(0).open(tx);
+		Index index = idxList.get(0).open(tx);
 		
 		// Insert 10 records with the same key
 		RecordId[] records = new RecordId[10];
@@ -117,35 +132,103 @@ public class HashIndexTest {
 		SearchKey int5 = new SearchKey(new IntegerConstant(5));
 		for (int i = 0; i < 10; i++) {
 			records[i] = new RecordId(blk, i);
-			cidIndex.insert(int5, records[i], false);
+			index.insert(int5, records[i], false);
 		}
 		
 		// Insert a record with another key
 		RecordId rid2 = new RecordId(blk, 9);
 		SearchKey int7 = new SearchKey(new IntegerConstant(7));
-		cidIndex.insert(int7, rid2, false);
+		index.insert(int7, rid2, false);
 		
 		// It should find 10 records for int 5
-		cidIndex.beforeFirst(new SearchRange(int5));
+		index.beforeFirst(new SearchRange(int5));
 		int k = 0;
-		while (cidIndex.next())
+		while (index.next())
 			k++;
 		assertTrue("*****HashIndexTest: bad insert", k == 10);
 		
 		// It should find only one record for int 7
-		cidIndex.beforeFirst(new SearchRange(int7));
-		cidIndex.next();
-		assertTrue("*****HashIndexTest: bad read index", cidIndex
+		index.beforeFirst(new SearchRange(int7));
+		index.next();
+		assertTrue("*****HashIndexTest: bad read index", index
 				.getDataRecordId().equals(rid2));
 		
 		// Delete the 10 records with key int 5
 		for (int i = 0; i < 10; i++)
-			cidIndex.delete(int5, records[i], false);
-		cidIndex.beforeFirst(new SearchRange(int5));
-		assertTrue("*****HashIndexTest: bad delete", cidIndex.next() == false);
+			index.delete(int5, records[i], false);
+		index.beforeFirst(new SearchRange(int5));
+		assertTrue("*****HashIndexTest: bad delete", index.next() == false);
 		
 		// Delete the record with key int 7
-		cidIndex.delete(int7, rid2, false);
-		cidIndex.close();
+		index.delete(int7, rid2, false);
+		index.close();
+	}
+	
+	@Test
+	public void testMultiKeys() {
+		
+		createMultiKeyIndex();
+		
+		List<IndexInfo> idxList = md.getIndexInfo(dataTableName, "cid", tx);
+		IndexInfo indexInfo = idxList.get(0);
+		Index index = indexInfo.open(tx);
+		
+		// Check the field names
+		assertTrue("*****HashIndexTest: bad index metadata",
+				indexInfo.fieldNames().contains("cid"));
+		assertTrue("*****HashIndexTest: bad index metadata",
+				indexInfo.fieldNames().contains("deptid"));
+		
+		// Insert 10 records with the same keys
+		BlockId blk = new BlockId(dataTableName + ".tbl", 0);
+		RecordId[] records1 = new RecordId[10];
+		SearchKey key1_1 = new SearchKey(new IntegerConstant(1), new IntegerConstant(1));
+		for (int i = 0; i < 10; i++) {
+			records1[i] = new RecordId(blk, i);
+			index.insert(key1_1, records1[i], false);
+		}
+		
+		// Insert 1 records with another key
+		blk = new BlockId(dataTableName + ".tbl", 1);
+		RecordId record2 = new RecordId(blk, 100);
+		SearchKey key2_1 = new SearchKey(new IntegerConstant(2), new IntegerConstant(1));
+		index.insert(key2_1, record2, false);
+		
+		// Insert 10 records with the third key
+		blk = new BlockId(dataTableName + ".tbl", 2);
+		RecordId[] records3 = new RecordId[10];
+		SearchKey key3_1 = new SearchKey(new IntegerConstant(3), new IntegerConstant(1));
+		for (int i = 0; i < 10; i++) {
+			records3[i] = new RecordId(blk, i);
+			index.insert(key3_1, records3[i], false);
+		}
+		
+		// It should find 10 records for the first key
+		index.beforeFirst(new SearchRange(key1_1));
+		int count = 0;
+		while (index.next())
+			count++;
+		assertTrue("*****HashIndexTest: bad insert", count == 10);
+		
+		// It should find only one record for the second key
+		index.beforeFirst(new SearchRange(key2_1));
+		index.next();
+		assertTrue("*****HashIndexTest: bad read index", index
+				.getDataRecordId().equals(record2));
+		
+		// It should find 10 records for the third key
+		index.beforeFirst(new SearchRange(key3_1));
+		count = 0;
+		while (index.next())
+			count++;
+		assertTrue("*****HashIndexTest: bad insert", count == 10);
+		
+		// Delete the records with the first key
+		for (int i = 0; i < 10; i++)
+			index.delete(key1_1, records1[i], false);
+		index.beforeFirst(new SearchRange(key1_1));
+		assertTrue("*****HashIndexTest: bad delete", index.next() == false);
+
+		index.close();
 	}
 }
