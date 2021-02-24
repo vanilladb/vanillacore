48ec654c8cd52564e2d14e916af682455c472d80
SLMT <sam123456777@gmail.com>
2021-02-24 14:29:11 +0800

    Improve Timers

--- src/main/java/org/vanilladb/core/util/TimerStatistics.java
+++ src/main/java/org/vanilladb/core/util/TimerStatistics.java
@@ -20,25 +20,26 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class TimerStatistics {
-	
-	private static final long REPORT_PERIOD = 3000;
-	private static final AtomicBoolean IS_REPORTING = new AtomicBoolean(false); 
-	
+
+	private static final AtomicBoolean IS_REPORTING = new AtomicBoolean(false);
+	private static final AtomicLong REPORT_PERIOD = new AtomicLong(10_000);
+
 	private static List<Object> components = new LinkedList<Object>();
 	private static Map<Object, ComponentStatistic> stats = new ConcurrentHashMap<Object, ComponentStatistic>();
 	private static Object[] anchors = new Object[1099];
-	
+
 	private static class ComponentStatistic {
 		long recordTime;
 		int lastCount, count;
-		
+
 		synchronized void addTime(long time) {
 			recordTime += time;
 			count++;
 		}
-		
+
 		synchronized double calAverage() {
 			double avg = 0;
 			if (count != 0) {
@@ -49,96 +50,106 @@ public class TimerStatistics {
 			count = 0;
 			return avg;
 		}
-		
+
 		synchronized int getLastCount() {
 			return lastCount;
 		}
 	}
-	
+
 	static {
 		for (int i = 0; i < anchors.length; i++)
 			anchors[i] = new Object();
-		
+
 		new Thread(new Runnable() {
 
 			@Override
 			public void run() {
 				long startTime = System.currentTimeMillis();
 				long lastTime = startTime;
-				
+
 				while (true) {
 					long currentTime = System.currentTimeMillis();
-					
-					if (currentTime - lastTime > REPORT_PERIOD) {
+
+					if (currentTime - lastTime > REPORT_PERIOD.get()) {
 						lastTime = currentTime;
-						
+
 						double t = (currentTime - startTime) / 1000;
 						StringBuilder sb = new StringBuilder();
-						
+
 						sb.append("===================================\n");
 						sb.append(String.format("At %.2f:\n", t));
-						
+
 						synchronized (components) {
 							for (Object key : components) {
 								ComponentStatistic stat = stats.get(key);
 								double avg = stat.calAverage();
 								int count = stat.getLastCount();
-								
+
 								if (count > 0) {
 									sb.append(String.format("%s: average %.2f us", key, avg));
 									sb.append(String.format(" with count %d\n", count));
 								}
 							}
 						}
-						
+
 						sb.append("===================================\n");
-						
+
 						if (IS_REPORTING.get())
 							System.out.println(sb.toString());
 					}
-					
+
 					try {
-						Thread.sleep(REPORT_PERIOD / 10);
+						Thread.sleep(REPORT_PERIOD.get() / 10);
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
 				}
 			}
-			
+
 		}).start();
 	}
-	
-	public static void startReporting() {
+
+	/**
+	 * Start periodically reporting.
+	 * 
+	 * @param reportPeriod the period of giving a report, in seconds.
+	 */
+	public static void startReporting(long reportPeriod) {
 		IS_REPORTING.set(true);
+		REPORT_PERIOD.set(reportPeriod * 1000);
 	}
-	
+
 	public static void stopReporting() {
 		IS_REPORTING.set(false);
 	}
-	
+
+	public static void addTime(Object com, long time) {
+		ComponentStatistic stat = stats.get(com);
+		if (stat == null)
+			stat = createNewStatistic(com);
+		stat.addTime(time);
+	}
+
 	static void add(Timer timer) {
 		if (IS_REPORTING.get()) {
 			for (Object com : timer.getComponents()) {
-				ComponentStatistic stat = stats.get(com);
-				if (stat == null)
-					stat = createNewStatistic(com);
-				stat.addTime(timer.getComponentTime(com));
+				addTime(com, timer.getComponentTime(com));
 			}
 		}
 	}
-	
+
 	private static Object getAnchor(Object key) {
 		int code = key.hashCode();
 		code = Math.abs(code); // avoid negative value
 		return anchors[code % anchors.length];
 	}
-	
+
 	private static ComponentStatistic createNewStatistic(Object key) {
 		Object anchor = getAnchor(key);
 		synchronized (anchor) {
-			
+
 			ComponentStatistic stat = stats.get(key);
-			
+
 			if (stat == null) {
 				stat = new ComponentStatistic();
 				stats.put(key, new ComponentStatistic());
@@ -146,7 +157,7 @@ public class TimerStatistics {
 					components.add(key);
 				}
 			}
-			
+
 			return stat;
 		}
 	}
