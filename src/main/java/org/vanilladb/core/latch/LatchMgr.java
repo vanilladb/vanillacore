97368ee38f25b987a93da94c80d5c619dfd3df56
Pin-Yu Wang <vito1218@gmail.com>
2022-01-03 22:30:57 +0800

    Refactor and add LatchContext

--- src/main/java/org/vanilladb/core/latch/LatchMgr.java
+++ src/main/java/org/vanilladb/core/latch/LatchMgr.java
@@ -3,34 +3,36 @@ package org.vanilladb.core.latch;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.vanilladb.core.latch.context.LatchContext;
+
 public class LatchMgr {
 	Map<String, Latch> latchMap = new HashMap<String, Latch>();
 
-	public ReentrantLatch registerReentrantLatch(String belonger, String function) {
-		StringBuilder nameBuilder = new StringBuilder();
-		nameBuilder.append(belonger);
-		nameBuilder.append(function);
-		
+	public static LatchContext newLatchContext() {
+		return new LatchContext();
+	}
+
+	public ReentrantLatch registerReentrantLatch(String caller, String callerMethod) {
 		ReentrantLatch latch = new ReentrantLatch();
-		latchMap.put(nameBuilder.toString(), latch);
+		latchMap.put(getKey(caller, callerMethod), latch);
 		return latch;
 	}
-	
-	public ReentrantReadWriteLatch registerReentrantReadWriteLatch(String belonger, String function) {
-		StringBuilder nameBuilder = new StringBuilder();
-		nameBuilder.append(belonger);
-		nameBuilder.append(function);
-		
+
+	public ReentrantReadWriteLatch registerReentrantReadWriteLatch(String caller, String callerMethod) {
 		ReentrantReadWriteLatch latch = new ReentrantReadWriteLatch();
-		latchMap.put(nameBuilder.toString(), latch);
+		latchMap.put(getKey(caller, callerMethod), latch);
 		return latch;
+	}
+
+	public String getKey(String caller, String callerMethod) {
+		return caller + callerMethod;
 	}
 
 	public void printLatchStats() {
 		for (String name : latchMap.keySet()) {
 			Latch latch = latchMap.get(name);
-			System.out.println(
-					String.format("%s, max: %d, tot: %d", name, latch.getMaxWaitingCount(), latch.getTotalAccessCount()));
+			System.out.println(String.format("%s, max: %d, tot: %d", name, latch.getMaxWaitingCount(),
+					latch.getTotalAccessCount()));
 		}
 	}
 }
