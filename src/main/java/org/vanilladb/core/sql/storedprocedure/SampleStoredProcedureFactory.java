abc279e9ca877cd66d3cb7296b36ca63af81d804
1018peter <peterconnected@gmail.com>
2022-03-07 19:32:09 +0800

    Fixed typo: getStroredProcedure -> getStoredProcedure

--- src/main/java/org/vanilladb/core/sql/storedprocedure/SampleStoredProcedureFactory.java
+++ src/main/java/org/vanilladb/core/sql/storedprocedure/SampleStoredProcedureFactory.java
@@ -18,7 +18,7 @@ package org.vanilladb.core.sql.storedprocedure;
 public class SampleStoredProcedureFactory implements StoredProcedureFactory {
 
 	@Override
-	public StoredProcedure<?> getStroredProcedure(int pid) {
+	public StoredProcedure<?> getStoredProcedure(int pid) {
 		throw new UnsupportedOperationException();
 	}
 
