8ca776518d0f3f1a1462c727ba0d36978776bf01
Pin-Yu Wang <vito1218@gmail.com>
2022-05-13 00:25:07 +0800

    Add two apis to get start and end timestamp of a component

--- src/main/java/org/vanilladb/core/util/TransactionProfiler.java
+++ src/main/java/org/vanilladb/core/util/TransactionProfiler.java
@@ -139,7 +139,14 @@ public class TransactionProfiler {
 			this.totalNetworkOutSize += networkOutSize;
 			this.invocationCount += invocationCount;
 		}
+		
+		private long getStartTime() { 
+			return elapsedTimeStart / 1000; 
+		}
 
+		private long getEndTime() { 
+			return totalElapsedTime + getStartTime(); 
+		}
 		
 		private long getTotalElapsedTime() {
 			return totalElapsedTime;
@@ -283,6 +290,20 @@ public class TransactionProfiler {
 		if (profiler == null)
 			return -1;
 		return profiler.getTotalElapsedTime();
+	}
+
+	public long getComponentStartTime(Object component) { 
+		SubProfiler profiler = subProfilers.get(component); 
+		if (profiler == null) 
+			return -1; 
+		return profiler.getStartTime(); 
+	}
+
+	public long getComponentEndTime(Object component) { 
+		SubProfiler profiler = subProfilers.get(component); 
+		if (profiler == null) 
+			return -1; 
+		return profiler.getEndTime(); 
 	}
 
 	public long getComponentCpuTime(Object component) {
